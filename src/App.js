fd2aa9caafd348aada29df8fa9c97c7d7f971ac3
theonlyouan <juanjovanegas21@gmail.com>
2022-01-09 18:55:03 -0500

    Added initial connection to Nasa's API using axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import PhotoItem from './components/PhotoItem';
 
 function App() {
+  const [dataNASA, setDataNASA] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get('https://api.nasa.gov/planetary/apod?api_key=Ajvrq2LlgIHkGKscdef5CdwzvO2kNSA3Q5ezWMDD&start_date=2022-01-05');
+        setDataNASA(response);
+        console.log(response);
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  const processedPhotosList = dataNASA.map(item => (
+    <PhotoItem 
+    title={item.title}
+    url={item.url}
+    explanation={item.explanation}
+    date={item.date}
+    copyright={item.copyright}
+    />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="App">
+        {loading && <p>Loading...</p>}
+        {!loading && processedPhotosList}
+      </div>
+    </div >
   );
 }
 
