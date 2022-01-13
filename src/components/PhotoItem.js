7bd79404f268376746c2fc2f6d9bb94117d08f9c
theonlyouan <juanjovanegas21@gmail.com>
2022-01-13 00:34:34 -0500

    Added styling to the 'interaction' section

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,25 +1,35 @@
 import PhotoCard from "./UI/PhotoCard";
 import classes from "./PhotoItem.module.css";
-import likeIcon from "../assets/heart.svg";
+import likeIcon from "../assets/likeIcon.svg";
+import unlikeIcon from "../assets/unlikeIcon.svg";
 
 const PhotoItem = (props) => {
     return (
         <PhotoCard>
-            <h3 className={classes.title}>{props.title}</h3>
-            <img className={classes.photo} src={props.url} alt={props.explanation} />
-            <p className={classes.cardText}>Date: {props.date}</p>
-            <p>Author: {props.copyright? props.copyright : 'None'}</p>
-            {/* If picture is liked, display the liked icon */}
-            {props.likeStatus &&
-                <button className={classes.likeButton}>❤</button>
-            }
-            {/* If picture is unliked, display the unliked icon */}
-            {!props.likeStatus &&
-                <button className={classes.likeButton}>
-                    <img src={likeIcon} className={classes.unlikeButton} />
-                </button>
-            }
-            <button>Learn More</button>
+            <h3>{props.title}</h3>
+            <img className={classes.photoBox} src={props.url} alt={props.explanation} />
+            <p className={classes.dateBox}>
+                <span style={{ fontWeight: "Bold" }}>Date: </span>
+                {props.date}
+            </p>
+            <p className={classes.authorBox}>
+                <span style={{ fontWeight: "Bold" }}>Author: </span>
+                {props.copyright ? props.copyright : 'None'}</p>
+            <div className={classes.interactionSection}>
+                {/* If picture is liked, display the liked icon */}
+                {props.likeStatus &&
+                    <button className={classes.likeButton}>
+                        <img src={likeIcon} className={classes.statusIcon}/>
+                    </button>
+                }
+                {/* If picture is unliked, display the unliked icon */}
+                {!props.likeStatus &&
+                    <button className={classes.likeButton}>
+                        <img src={unlikeIcon} className={classes.statusIcon}/>
+                    </button>
+                }
+                <button className={classes.learnMoreButton}>Learn More &#187;</button>
+            </div>
         </PhotoCard>
     )
 };
